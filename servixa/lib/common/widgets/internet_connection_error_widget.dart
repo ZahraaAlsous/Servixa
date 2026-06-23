75b3e337a9b61d87c909e18fc42e6bd4d0d46ebd
Zahraa_Alsous <alsous.zahraa@gmail.com>
2026-06-23 10:27:23 +0300

    download lottie library and make animation for error net widget

--- servixa/lib/common/widgets/internet_connection_error_widget.dart
+++ servixa/lib/common/widgets/internet_connection_error_widget.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:servixa/core/const/theme_app.dart';
 import 'package:servixa/core/const/typography_app.dart';
+import 'package:lottie/lottie.dart';
 
 class InternetConnectionErrorWidget extends StatelessWidget {
   final void Function()? onPressed;
@@ -13,12 +14,17 @@ class InternetConnectionErrorWidget extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Icon(
-            Icons.wifi_off,
-            size: 64,
-            color: ThemeApp.Foundation_Secendary_grey_100,
+          // const Icon(
+          //   Icons.wifi_off,
+          //   size: 64,
+          //   color: ThemeApp.Foundation_Secendary_grey_100,
+          // ),
+          Lottie.asset(
+            'assets/animations/net.json',
+            width: 100,
+            height: 100,
           ),
-          const SizedBox(height: 16),
+          // const SizedBox(height: 16),
           Text(
             'No internet connection or server error'.tr(),
             style: TypographyApp.Title_Mid_Regular.copyWith(
